178b56b81ddf4ae4c2745d9a7e38536d2a1ba6e5
izmhr <atsizumi@gmail.com>
2018-12-10 00:22:56 +0900

    1st commit

---
+++ Assets/ChangeMask.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMask : MonoBehaviour
+{
+    Camera cam;
+    LayerMask layerL;
+    LayerMask layerR;
+    bool toggle = false;
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        layerL = LayerMask.NameToLayer("L");
+        layerR = LayerMask.NameToLayer("R");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            toggle = !toggle;
+            if (toggle)
+            {
+                cam.cullingMask = (1 << layerL);
+            }
+            else
+            {
+                cam.cullingMask = (1 << layerR);
+            }
+            
+        }
+    }
+}
